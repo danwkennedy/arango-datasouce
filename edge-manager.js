b6989499efad3f3607936bd785f8d9bb682900dc
danwkennedy <danwkennedy@gmail.com>
2019-06-13 20:06:26 -0400

    feat(edge manager): add bulk operations

--- edge-manager.js
+++ edge-manager.js
@@ -25,7 +25,7 @@ class EdgeManager extends DataSource {
    *
    * @param {String} from The id of the origin document
    * @param {String} to The id of the destination document
-   * @param {*} [properties={}] An object with key values describing the relationship
+   * @param {Object} [properties={}] An object with key values describing the relationship
    * @returns An object with the new version of the edge
    * @memberof EdgeManager
    */
@@ -34,6 +34,65 @@ class EdgeManager extends DataSource {
       returnNew: true
     });
     return { new: edge };
+  }
+
+  /**
+   * Create a one to many to one relationship. This is useful when bulk operations are possible.
+   *
+   * @param {String} fromId The id of the single document
+   * @param {String[]} toIds An array of ids to the many documents
+   * @param {Function} [getProperties=() => ({})] A function that resolves the extra properties of each edge
+   * @param {Object} [opts={}] Options to pass to the create call
+   * @returns {Object[]} An array of the created edges
+   * @memberof EdgeManager
+   */
+  async createOneToMany(fromId, toIds, getProperties = () => ({}), opts = {}) {
+    const edges = toIds.map(toId => ({
+      _from: fromId,
+      _to: toId,
+      ...getProperties(fromId, toId)
+    }));
+
+    return this.createMany(edges, opts);
+  }
+
+  /**
+   * Create a many to one relationship. This is useful when bulk operations are possible.
+   *
+   * @param {String[]} fromIds The ids of the many documents
+   * @param {String} toId The id of the single document
+   * @param {Function} [getProperties=() => ({})] A function that resolves the extra properties of each edge
+   * @param {Object} [opts={}] Options to pass to the create call
+   * @returns {Object[]} An array of the created edges
+   * @memberof EdgeManager
+   */
+  async createManyToOne(fromIds, toId, getProperties = () => ({}), opts = {}) {
+    const edges = fromIds.map(fromId => ({
+      _from: fromId,
+      _to: toId,
+      ...getProperties(fromId, toId)
+    }));
+    return this.createMany(edges, opts);
+  }
+
+  /**
+   * Create many edges in a single database call. This is useful when bulk operations are possible.
+   *
+   * @param {Object[]} edges The edges to create
+   * @returns {Object[]} The created edges
+   * @memberof EdgeManager
+   */
+  async createMany(edges) {
+    const query = aql`
+        FOR edge in ${edges}
+            UPSERT { _from: edge._from, _to: edge._to }
+                INSERT edge
+                UPDATE {}
+            IN ${aql.literal(this.collection.name)}
+            RETURN NEW
+    `;
+
+    return this.db.query(query);
   }
 
   /**
@@ -58,6 +117,26 @@ class EdgeManager extends DataSource {
     }
 
     return { old: edge };
+  }
+
+  /**
+   * Removes multiple edges by filtering on their _from and _to handles. This is useful when bulk operations are possible.
+   *
+   * @param {*} edges
+   * @returns
+   * @memberof EdgeManager
+   */
+  async removeMany(edges) {
+    const query = aql`
+        FOR edge IN ${edges}
+            FOR collectionEdge IN ${this.collection}
+                FILTER collectionEdge._from == edge._from
+                FILTER collectionEdge._to == edge._to
+                REMOVE collectionEdge IN ${aql.literal(this.collection.name)}
+            RETURN OLD
+      `;
+
+    return this.db.query(query);
   }
 }
 
--- edge-manager.test.js
+++ edge-manager.test.js
@@ -30,27 +30,82 @@ describe(`EdgeManager`, () => {
     dbMock.reset();
   });
 
-  test(`it calls collection.save()`, async () => {
-    const doc = { name: 'abc' };
-    const from = 'abc';
-    const to = 'xyz';
-    const properties = { test: true };
-    collectionMock.save.mockResolvedValue({ new: doc });
+  describe(`EdgeManager.create`, () => {
+    test(`it calls collection.save()`, async () => {
+      const doc = { name: 'abc' };
+      const from = 'abc';
+      const to = 'xyz';
+      const properties = { test: true };
+      collectionMock.save.mockResolvedValue({ new: doc });
 
-    const output = await manager.create(from, to, properties);
-    expect(output).toEqual({ new: doc });
-    expect(collectionMock.save).toHaveBeenCalledTimes(1);
-    expect(collectionMock.save).toHaveBeenCalledWith(properties, from, to, {
-      returnNew: true
+      const output = await manager.create(from, to, properties);
+      expect(output).toEqual({ new: doc });
+      expect(collectionMock.save).toHaveBeenCalledTimes(1);
+      expect(collectionMock.save).toHaveBeenCalledWith(properties, from, to, {
+        returnNew: true
+      });
     });
   });
 
-  test(`it calls collection.remove()`, async () => {
-    const edge = { name: 'abc' };
-    dbMock.query.mockResolvedValue(edge);
+  describe(`EdgeManager.remove`, () => {
+    test(`it calls db.query()`, async () => {
+      const edge = { name: 'abc' };
+      dbMock.query.mockResolvedValue(edge);
 
-    const output = await manager.remove('123');
-    expect(output).toEqual({ old: edge });
-    expect(dbMock.query).toHaveBeenCalledTimes(1);
+      const output = await manager.remove('123');
+      expect(output).toEqual({ old: edge });
+      expect(dbMock.query).toHaveBeenCalledTimes(1);
+    });
+
+    test(`it throws when the edge doesn't exist`, async () => {
+      dbMock.query.mockResolvedValue(null);
+      await expect(manager.remove('123')).rejects.toThrow();
+    });
+  });
+
+  describe(`EdgeManager.createMany`, () => {
+    test(`it calls db.query`, async () => {
+      await manager.createMany([{ _from: '123', _to: '123', test: true }]);
+      expect(dbMock.query).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe(`EdgeManager.createOneToMany`, () => {
+    test(`it calls getProperties() for every edge`, async () => {
+      const getProperties = jest.fn();
+      await manager.createOneToMany('123', ['456', '789'], getProperties);
+
+      expect(getProperties).toHaveBeenCalledTimes(2);
+      expect(getProperties).toHaveBeenNthCalledWith(1, '123', '456');
+      expect(getProperties).toHaveBeenNthCalledWith(2, '123', '789');
+    });
+
+    test(`it calls db.query`, async () => {
+      await manager.createOneToMany('123', ['456', '789']);
+      expect(dbMock.query).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe(`EdgeManager.createManyToOne`, () => {
+    test(`it calls getProperties() for every edge`, async () => {
+      const getProperties = jest.fn();
+      await manager.createManyToOne(['456', '789'], '123', getProperties);
+
+      expect(getProperties).toHaveBeenCalledTimes(2);
+      expect(getProperties).toHaveBeenNthCalledWith(1, '456', '123');
+      expect(getProperties).toHaveBeenNthCalledWith(2, '789', '123');
+    });
+
+    test(`it calls db.query`, async () => {
+      await manager.createManyToOne(['456', '789'], '123');
+      expect(dbMock.query).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe(`EdgeManager.removeMany`, () => {
+    test(`it calls db.query`, async () => {
+      await manager.removeMany([{ _from: '123', _to: '123', test: true }]);
+      expect(dbMock.query).toHaveBeenCalledTimes(1);
+    });
   });
 });
